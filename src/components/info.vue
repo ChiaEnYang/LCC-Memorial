164ab6e1fb1f09a5925c139ff5ea12b6865c4ff1
popcorn-pather <gc178694@yahoo.com.tw>
2024-10-04 13:43:09 +0800

    footer

---
+++ src/components/info.vue
@@ -0,0 +1,38 @@
+<script>
+</script>
+
+<template>
+    
+    <footer class="info w-screen h-auto p-20 display: flex flex-wrap justify-around items-center bg-[url('/src/img/mountain.png')] bg-cover bg-/[.06]">
+        <div class="map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.8676070038923!2d120.67096857501495!3d24.141287178405673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d0cf25b4517%3A0xc6a3c93dc5084762!2z5p6X5LmL5Yqp57SA5b-16aSo!5e0!3m2!1szh-TW!2stw!4v1728008463834!5m2!1szh-TW!2stw"
+                width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
+        <div class="place bg-transparent w-500 h-4/5 my-20 ">
+            <div class="logo w-[200px] h-[50px] bg-contain bg-no-repeat bg-[url('/src/img/logo.png')]"></div>
+            <div class="text text-start">
+                <div >開館時間： 週二~週六．11:00~17:00 </div>
+                <div>週日、週一及國定假日休館</div>
+                <div>聯絡電話： (04)2218-3652</div>
+                <div>聯絡地址： 403 臺中市西區柳川西路二段158號、162號 </div>
+                <div>電子信箱： chihchu@gm.ntcu.edu.tw </div>
+                <div>粉絲專頁： <a  href="https://www.facebook.com/LinChihChuMemorial/">林之助紀念館</a></div>
+
+            </div>
+        </div>
+        
+    </footer>
+    <div class="w-screen text-center bg-slate-400">Copyright © 2024林之助紀念館 / 2024 Lin, Chih-Chu Memorial. All rights reserved.</div>
+
+</template>
+
+<style>
+  .gm-control-active{
+    width: 20px!important;
+    height: 20px!important;
+  }
+  
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <script>
 import banner from "@/components/banner.vue";
+import info from "@/components/info.vue";
 
 export default {
-    components: {
-      banner,
-    }
+  components: {
+    banner,
+    info
+  }
 }
 </script>
 
 <template>
   <main class="container mx-auto pt-20">
     <banner />
+    <info />
   </main>
+  <footer class="container mx-auto pt-20">
+    
+    <info />
+  </footer>
+
+
+
 </template>
