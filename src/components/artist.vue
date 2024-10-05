1ae5d5317f20c4a3e0318b561308e663203a1670
popcorn-pather <gc178694@yahoo.com.tw>
2024-10-05 15:18:33 +0800

    new

--- src/components/artist.vue
+++ src/components/artist.vue
@@ -12,11 +12,14 @@
 
 <template>
     <div class="w-full h-screen flex justify-center">
-        <div class="w-full h-full  bg-[url('/src/img/lcc-profile-bk.PNG')] bg-contain bg-no-repeat bg-center flex py-40 px-28 pl-0">
-            <div class="artist w-1/4 h-full bg-[url('/src/img/lcc-head.PNG')] bg-cover bg-no-repeat bg-center"></div>
-            <div class="arttext w-3/4 h-full py-20 px-28">
+        <div class="content w-full h-full  bg-[url('/src/img/lcc-profile-bk.png')] bg-contain bg-no-repeat bg-center flex justify-center py-40 pr-5 pl-9 ">
+            <div class="artist w-1/6 h-full ">
+                <div class=" w-full h-full bg-[url('/src/img/lcc-head.png')] bg-cover bg-no-repeat bg-center "></div>
+            </div>
+            <div class="arttext w-3/6 h-full py-20 px-28">
                 <h1 class="text-center font-bold mb-3">林之助（1917-2008）</h1>
-                出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。</div>
+                出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。
+            </div>
         </div>
         
     </div>
