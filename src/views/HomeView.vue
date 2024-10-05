04ad453d8dde9c64be3446fa483f731a5e44095c
popcorn-pather <gc178694@yahoo.com.tw>
2024-10-05 10:47:40 +0800

    new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,15 @@ import banner from "@/components/banner.vue";
 import info from "@/components/info.vue";
 import about from "@/components/aboutCards.vue";
 import model from "@/components/model.vue";
+import artist from "@/components/artist.vue";
 
 export default {
   components: {
     banner,
     about,
     info,
-    model
+    model,
+    artist
   }
 }
 </script>
@@ -17,6 +19,7 @@ export default {
 <template >
   <main class="container mx-auto pt-20">
     <banner />
+    <artist />
     <about />
     <model />
   </main>
