df8393d2adefa8e1bbebfff19856a2e6cf805f6e
ChiaEnYang <grace86716@gmail.com>
2024-10-12 15:58:29 +0800

    林之助生平卡片滾動特效

--- src/components/artist.vue
+++ src/components/artist.vue
@@ -8,50 +8,41 @@ export default {
             artistDescribe: [
                 {
                     id: 'describe-1',
-                    content: '1出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。'
+                    content: `出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。`
                 },
                 {
                     id: 'describe-2',
-                    content: '2出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。'
+                    content: `1945 年第二次世界大戰結束，臺灣經歷政權轉換，以膠為媒劑的東洋畫在戰後的臺灣日漸式微、幾近失傳。林之助有感於「東洋畫」名稱帶日本風，難容於當時的官方意識形態，於1977年提出「膠彩畫正名」，改用繪畫材料命名，以「膠彩畫」取代「東洋畫」，避免多餘的誤解與爭議，其後更組織膠彩畫協會、舉辦膠彩畫展，使膠彩畫在臺灣美術中得以保留並延續發展。`
                 },
                 {
                     id: 'describe-3',
-                    content: '3出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。'
-                },
-                {
-                    id: 'describe-4',
-                    content: '4出生於臺中大雅，12歲赴日本東京留學，中學畢業後考進日本帝國美術學校（今武藏野美術大學）日本畫科就讀，受教於奧村土牛、山口逢春、川崎小虎及小林巢居。畢業後進入兒玉希望日本畫塾，深入學習當時日本畫官展繪畫風格，並在年僅24歲時以〈朝涼〉入選紀元二六○○年奉祝美術展。林之助返臺後參加臺灣官辦府展，1942年以〈母子〉獲第五屆府展特選，1943年〈好日〉再獲第六屆府展特選第一名，奠定在臺灣畫壇的地位。'
+                    content: `林之助於1946年進入臺中師範學校（今國立臺中教育大學）執教，同時遷入位於柳川西路的學校附屬宿舍，自此展開長達一甲子、他與住家兼畫室的不解之緣。\n由於當時學校裡只能教授素描、水彩及色彩學課程，面對膠彩畫即將失傳的窘境，林之助採用傳統畫塾的方式，從每屆學生中挑選出優秀學生，非但不收學費且免費提供顏料繪具，課後於住處教導傳授，以此培養膠彩畫的新一代。也因林之助所住宿舍外圍為竹籬笆圍牆，學生們便將之暱稱為「竹籬笆畫室」。這些竹籬笆畫室的學生，成了膠彩畫沒落、艱困時期中的少數捍衛者，也是往後臺灣省膠彩畫協會成立主要的中堅會員及推動執行者。`
                 }
             ]
         }
     },
     mounted() {
-        // Register GSAP Plugins
+        // Register GSAP Plugins 將要用的套件加進來
         gsap.registerPlugin(ScrollTrigger, Observer);
 
-        // Timeline Animations
-        let tl1In = gsap.timeline();
-        tl1In.from("#describe-1", { y: "30%", opacity: 0, duration: 0.75, ease: "power2.inOut" });
-
+        // Timeline Animations 設定動畫細節
         let tl1Out = gsap.timeline();
-        tl1Out.to("#describe-1", { y: "-30%", opacity: 0, duration: 1, ease: "power2.inOut" });
+        tl1Out.fromTo("#describe-2", { y: "30%", opacity: 0, duration: 0.3, ease: "power2.inOut" }, { y: "10%", opacity: 1, duration: 0.3, ease: "power2.inOut" }, 0);
+        tl1Out.to("#describe-1", { y: "-10%", scale: 0.9, opacity: 0.3, duration: 0.3, ease: "power2.inOut" }, 0);
 
         let tl2In = gsap.timeline();
-        tl2In.from("#describe-2", { y: "30%", opacity: 0, duration: 0.75, ease: "power2.inOut" });
+        tl2In.to("#describe-1", { y: "-30%", opacity: 0, duration: 0.3, ease: "power2.inOut" }, 0);
+        tl2In.to("#describe-2", { y: "0", duration: 0.3, ease: "power2.inOut" }, 0);
 
         let tl2Out = gsap.timeline();
-        tl2Out.to("#describe-2", { y: "-30%", opacity: 0, duration: 1, ease: "power2.inOut" });
+        tl2Out.fromTo("#describe-3", { y: "30%", opacity: 0, duration: 0.3, ease: "power2.inOut" }, { y: "10%", opacity: 1, duration: 0.3, ease: "power2.inOut" }, 0);
+        tl2Out.to("#describe-2", { y: "-10%", scale: 0.9, opacity: 0.3, duration: 0.3, ease: "power2.inOut" }, 0);
 
         let tl3In = gsap.timeline();
-        tl3In.from("#describe-3", { y: "30%", opacity: 0, duration: 0.75, ease: "power2.inOut" });
+        tl3In.to("#describe-2", { y: "-30%", opacity: 0, duration: 0.3, ease: "power2.inOut" }, 0);
+        tl3In.to("#describe-3", { y: "0", duration: 0.3, ease: "power2.inOut" }, 0);
 
-        let tl3Out = gsap.timeline();
-        tl3Out.to("#describe-3", { y: "-30%", opacity: 0, duration: 1, ease: "power2.inOut" });
-
-        let tl4In = gsap.timeline();
-        tl4In.from("#describe-4", { y: "30%", opacity: 0, duration: 0.75, ease: "power2.inOut" });
-
-        // Master Timeline
+        // Master Timeline 把所有時間軸整合起來
         let masterTL = gsap.timeline({
             onReverseComplete: () => {
                 scrollObserver.disable();
@@ -63,9 +54,10 @@ export default {
             },
             paused: true
         });
-        masterTL.add(tl1In, "in1").addPause().add(tl2In, "in2").add(tl1Out, "out1").addPause().add(tl3In, "in3").add(tl2Out, "out2").addPause().add(tl4In, "in4").add(tl3Out, "out3");
+        // 排列播放的順序
+        masterTL.add(tl1Out, "out1").addPause().add(tl2In, "in2").addPause().add(tl2Out, "out2").addPause().add(tl3In, "in3");
 
-        // Scroll Observer
+        // Scroll Observer 監聽網頁的滾輪事件
         let scrollObserver = Observer.create({
             type: "wheel,touch",
             wheelSpeed: -1,
@@ -115,14 +107,14 @@ export default {
 <template>
     <!-- 
      把區塊切成左右兩格，右邊留給雕像 -->
-    <div id="artist-section" class="w-[90%] h-screen grid grid-cols-2">
+    <div id="artist-section" class="w-[90%] h-dvh grid grid-cols-2">
         <!-- 
         文字區 -->
         <div class="border flex flex-col items-center">
             <h3 class="w-full h-1/4 pl-16 text-5xl font-bold border content-center">林之助</h3>
-            <div class="w-80 text-justify relative">
-                <div class="w-100 absolute top-0 left-0 text-lg bg-white" v-for="text in artistDescribe" :key="text.id"
-                    :id="text.id">
+            <div class="w-3/5 text-justify relative">
+                <div class="w-100 absolute top-0 left-0 text-lg font-medium whitespace-pre-line"
+                    v-for="text in artistDescribe" :key="text.id" :id="text.id">
                     {{ text.content }}
                 </div>
             </div>
